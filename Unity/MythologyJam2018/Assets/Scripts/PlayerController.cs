968855c0dc6f468d923ec13f5fe0369b33be18c3
BOGOSLAF\Henry <henry.schaefer@outlook.com>
2018-09-18 20:54:22 -0700

    added shit

---
+++ Unity/MythologyJam2018/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 forward;
+    private Vector3 right;
+
+    void Start()
+    {
+        forward = Camera.main.transform.forward;
+        forward = new Vector3(Camera.main.transform.forward.y, 0f, Camera.main.transform.forward.z);
+    }
+
+    void Update()
+    {
+
+    }
+}
