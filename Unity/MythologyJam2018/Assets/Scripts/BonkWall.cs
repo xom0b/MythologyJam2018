c825484fb0ceb9d80c1a518ffbabdcb90151040b
BOGOSLAF\Henry <henry.schaefer@outlook.com>
2018-11-03 17:00:45 -0700

    added bonk walls

---
+++ Unity/MythologyJam2018/Assets/Scripts/BonkWall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonkWall : MonoBehaviour
+{
+    public Vector3 wallNormal;
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(transform.position, wallNormal, Color.blue);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("I: " + transform.parent.gameObject.name + " hit: " + collision.gameObject.name);
+    }
+}
